d9ec57c6d56d783f40b33965dc477f8c0db71285
Marcos Curvello <mrcurvello@gmail.com>
2026-09-08 13:10:11 +0100

    feat(app): implement app configuration

---
+++ PhotoFeed/App/AppConfiguration.swift
@@ -0,0 +1,27 @@
+//
+//  AppConfiguration.swift
+//  PhotoFeed
+//
+//  Created by Marcos Curvello on 08/09/2026.
+//
+
+import Foundation
+
+nonisolated enum AppConfigurationError: Error, Equatable {
+    case missingUnsplashAccessKey
+}
+
+nonisolated struct AppConfiguration: Sendable {
+    
+    let unsplashAccessKey: String
+    
+    init(bundle: Bundle = .main) throws {
+        guard let accessKey = bundle.object(forInfoDictionaryKey: "UNSPLASH_ACCESS_KEY") as? String,
+              !accessKey.isEmpty,
+              accessKey != "$(UNSPLASH_ACCESS_KEY)" else {
+            throw AppConfigurationError.missingUnsplashAccessKey
+        }
+        
+        self.unsplashAccessKey = accessKey
+    }
+}
