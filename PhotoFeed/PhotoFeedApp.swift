f449bc6460165ad3f485211a78be7e2b9dad92fb
Marcos Curvello <mrcurvello@gmail.com>
2026-09-08 11:58:00 +0100

    Initial commit

---
+++ PhotoFeed/PhotoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFeedApp.swift
+//  PhotoFeed
+//
+//  Created by Marcos Curvello on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
